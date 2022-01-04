0e3981a14f729f9485cdbf365eb5c95ef75049a8
Chetan <18bet1051@gmail.com>
2022-01-04 14:18:17 +0530

    New Question and Answer added(pattern6)

---
+++ PatternQuestions/src/Pattern6.java
@@ -0,0 +1,39 @@
+/*
+  
+
+Ques 6:
+n = 5
+* * * * *
+	* * * *
+		* * *
+			* *
+				*
+
+
+ */
+import java.util.Scanner;
+public class Pattern6 {
+	public static void main(String h[]) {
+		System.out.println("Enter the value of n");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		method1(n);
+	}
+	
+	static void method1(int n ) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i <= j) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+			for(int j = 0; j <= i; j++) {
+				System.out.print("  ");
+			}
+		}
+	}
+}
