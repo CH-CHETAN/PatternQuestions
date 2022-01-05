43e3e1e41d18ba1d00aa6b5b3a995ca3a2639eca
Chetan <18bet1051@gmail.com>
2022-01-05 11:23:42 +0530

    New Question and Answer added(Pattern8)

---
+++ PatternQuestions/src/Pattern8.java
@@ -0,0 +1,36 @@
+/*
+ 
+ 
+ Ques 8:
+n = 5
+* 		*
+  *   *
+    *
+  *   *
+*       *
+ 
+
+ */
+import java.util.Scanner;
+public class Pattern8 {
+	public static void main(String h[]) {
+		System.out.println("Enter the value of n");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		method1(n);
+	}
+	
+	static void method1(int n) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i == j || i + j == n-1) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
