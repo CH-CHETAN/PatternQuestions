e7e37586f208d1cbefbe879b756d68204d7a2741
Chetan <18bet1051@gmail.com>
2022-01-05 11:33:54 +0530

    New Question and Answer added(Pattern9)

---
+++ PatternQuestions/src/Pattern9.java
@@ -0,0 +1,43 @@
+/*
+  
+ 
+Ques 9:
+n = 5
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+ 
+ */
+import java.util.Scanner;
+public class Pattern9 {
+	public static void main(String h[]) {
+		System.out.println("Enter the value of n");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		method1(n);
+	}
+	
+	static void method1(int n) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n;j++) {
+				if(i + j >= n-1) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			for(int j = 0; j < n; j++) {
+				if(i > j) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
